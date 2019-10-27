33783a3763a46c16d6354b95f7b99beebe1860ed
Oscar Pons <ospoga9@gmail.com>
2019-10-27 15:04:15 +0100

    Update ModuleEditor.cpp

--- Source/ModuleEditor.cpp
+++ Source/ModuleEditor.cpp
@@ -587,19 +587,19 @@ void ModuleEditor::DrawCreateMenu()
 	if (ImGui::BeginMenu("Basic shapes"))
 	{
 		if (ImGui::MenuItem("Cylinder"))
-			App->resources->CreateShape(CYLINDER, 9, 9);
+			App->resources->CreateShape(CYLINDER, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Cone"))
-			App->resources->CreateShape(CONE, 9, 9);
+			App->resources->CreateShape(CONE, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Sphere"))
-			App->resources->CreateShape(SPHERE, 9, 9);
+			App->resources->CreateShape(SPHERE, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Plane"))
-			App->resources->CreateShape(PLANE, 9, 9);
+			App->resources->CreateShape(PLANE, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Cube"))
-			App->resources->CreateShape(CUBE, 9, 9);
+			App->resources->CreateShape(CUBE, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		ImGui::EndMenu();
 	}
@@ -607,31 +607,31 @@ void ModuleEditor::DrawCreateMenu()
 	if (ImGui::BeginMenu("Extended shapes"))
 	{
 		if (ImGui::MenuItem("Torus"))
-			App->resources->CreateShape(TORUS, 9, 9);
+			App->resources->CreateShape(TORUS, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Bottle"))
-			App->resources->CreateShape(BOTTLE, 9, 9);
+			App->resources->CreateShape(BOTTLE, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Knot"))
-			App->resources->CreateShape(KNOT, 9, 9);
+			App->resources->CreateShape(KNOT, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Hemisphere"))
-			App->resources->CreateShape(HEMISPHERE, 9, 9);
+			App->resources->CreateShape(HEMISPHERE, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Icosahedron"))
-			App->resources->CreateShape(ICOSAHEDRON, 9, 9);
+			App->resources->CreateShape(ICOSAHEDRON, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Dodecahedron"))
-			App->resources->CreateShape(DODECAHEDRON, 9, 9);
+			App->resources->CreateShape(DODECAHEDRON, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Octahedron"))
-			App->resources->CreateShape(OCTAHEDRON, 9, 9);
+			App->resources->CreateShape(OCTAHEDRON, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Tetrahedron"))
-			App->resources->CreateShape(TETRAHEDRON, 9, 9);
+			App->resources->CreateShape(TETRAHEDRON, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		if (ImGui::MenuItem("Rock"))
-			App->resources->CreateShape(ROCK, 9, 9);
+			App->resources->CreateShape(ROCK, 9, 9,0,0,0,0.5f, App->scene->GetSelectedGameobj()->GetUID());
 
 		ImGui::EndMenu();
 	}
