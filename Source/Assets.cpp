37181533f9e349906baa173897251e4afd3a237b
DavidTello1 <36265057+DavidTello1@users.noreply.github.com>
2019-11-05 13:45:40 +0100

    Assets frame

--- Source/Assets.cpp
+++ Source/Assets.cpp
@@ -11,7 +11,7 @@ Assets::Assets() : Panel("Assets")
 	pos_x = default_pos_x;
 	pos_y = default_pos_y;
 
-	active = false; //remove when panel assets is not empty
+	has_menubar = true;
 }
 
 Assets::~Assets()
@@ -20,13 +20,58 @@ Assets::~Assets()
 
 void Assets::Draw()
 {
-	ImGui::BeginChild("Hierarchy", ImVec2(ImGui::GetWindowContentRegionWidth() * 0.2f, ImGui::GetWindowHeight()*0.8f), true);
-	//...
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("Create"))
+		{
+			ImGui::MenuItem("Folder");
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::MenuItem("Remove", nullptr, false, !selected_assets.empty()))
+		{
+		}
+
+		if (ImGui::MenuItem("Rename", nullptr, false, !selected_assets.empty()))
+		{
+
+		}
+
+		ImGui::EndMenuBar();
+	}
+
+	// Child Hierarchy
+	ImGui::BeginChild("Hierarchy", ImVec2(ImGui::GetWindowContentRegionWidth() * 0.2f, 0), true);
+	
+	if (ImGui::TreeNode("Show All"))
+	{
+		ImGui::Selectable("Meshes");
+		ImGui::Selectable("Materials");
+		ImGui::Selectable("Models");
+
+		ImGui::TreePop();
+	}
+
+	ImGui::Separator();
+
+	if (ImGui::TreeNode("Assets"))
+	{
+		ImGui::TreePop();
+	}
 	ImGui::EndChild();
 	ImGui::SameLine();
 
-	ImGui::BeginChild("Icons", ImVec2(ImGui::GetWindowContentRegionWidth() * 0.78f, ImGui::GetWindowHeight()*0.8f), true);
-	//...
+
+	// Child Icons
+	ImGui::BeginChild("Icons", ImVec2(0, 0), true, ImGuiWindowFlags_MenuBar);
+	
+	if (ImGui::BeginMenuBar())
+	{
+		//ImGui::Text("%s>%s", folder_name.parent, folder_name.parent.parent, ...);
+
+		ImGui::EndMenuBar();
+	}
+
 	ImGui::EndChild();
 }
 
--- Source/Assets.h
+++ Source/Assets.h
@@ -19,6 +19,9 @@ public:
 
 	void Draw();
 	void ImportAsset();
+
+private:
+	std::vector<uint> selected_assets;
 };
 
 #endif// __ASSETS_H__
